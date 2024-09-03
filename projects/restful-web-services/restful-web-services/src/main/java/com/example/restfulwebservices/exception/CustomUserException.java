b6aaef1e85210c1f99c82d71636523ec45de8ec8
Samy <misamy@deloitte.com>
2024-09-03 12:29:02 +0300

    adding zip empty project

--- projects/restful-web-services/restful-web-services/src/main/java/com/example/restfulwebservices/exception/CustomUserException.java
+++
@@ -1,8 +0,0 @@
-package com.example.restfulwebservices.exception;
-
-public class CustomUserException  extends  RuntimeException {
-
-    public CustomUserException(String message) {
-        super(message);
-    }
-}
